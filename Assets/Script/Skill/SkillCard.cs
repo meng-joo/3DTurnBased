3c5f6c22775c94794ced8773545e17bdb8c820be
meng-joo <81345410+meng-joo@users.noreply.github.com>
2022-11-21 18:37:31 +0900

    전투 카드 정보 불러와야함

---
+++ Assets/Script/Skill/SkillCard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SkillCard : MonoBehaviour
+{
+    [Header("스킬 카드 요소")]
+    public Image backGroundImage;
+    public Image skillImage;
+    public TextMeshProUGUI skillName;
+    public TextMeshProUGUI skillInfo;
+
+    private void Start()
+    {
+        backGroundImage = GetComponent<Image>();
+        skillImage = transform.GetChild(0).GetComponent<Image>();
+        skillName = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        skillInfo = transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetSkillCard(Skill skillData)
+    {
+        backGroundImage.color = skillData.skillInfo._skillCardColor;
+        skillImage.sprite = skillData.skillInfo._skillImage;
+        skillName.text = skillData.skillInfo._skillName;
+        skillInfo.text = skillData.skillInfo._skillExplanation;
+    }
+}
